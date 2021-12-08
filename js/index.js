b8fe571b497cb6b5ecbb75eca0fa78ed64840ccc
Mikhail Nosov <manosov1984@gmail.com>
2021-12-08 17:03:48 +0400

    second day

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import slideMenu from "./menu.js";
+import renderVideo from "./renderVideo.js";
   
 slideMenu({
   openBtn: '.header__burger-btn',
@@ -7,3 +8,5 @@ slideMenu({
   closeTrigger:  '.navigation__link, .navigation__close',
 });
 
+
+renderVideo();
---
+++ js/services.js
@@ -0,0 +1,21 @@
+const API_KEY = '4383dc2332f67add4b81849ca3c4c270';
+const BASE_URL = 'https://api.themoviedb.org/3/';
+const LANGUAGE = '&language=ru-RU';
+
+//trending/all/day?api_key=<<api_key>>
+
+const getData = (url) => fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json()
+    }
+    throw `Что-то пошло не так, ошибка ${response.status}`
+  })
+  .catch (err => console.log(err));
+
+
+
+export const getTrends = async (type = 'all', period = 'day', page = '1') => {
+  const url =`${BASE_URL}trending/${type}/${period}?api_key=${API_KEY}${LANGUAGE}&page=${page}`;
+   return await getData(url);
+}
